b2535b8b63c6d185d83eb1061638f912c0fac630
Konstantin Hantsov <hantsov@cantemo.com>
2020-02-19 18:48:11 +0100

    Solve Sum problem

--- module01/sum.go
+++ module01/sum.go
@@ -3,5 +3,11 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	return 0
+	s := 0
+
+	for _, el := range numbers {
+		s = s + el
+	}
+
+	return s
 }
